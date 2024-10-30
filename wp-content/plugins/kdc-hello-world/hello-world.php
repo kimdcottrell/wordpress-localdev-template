75f07943a9ae4e57d490e86b6a30c5c5b9785968
kimdcottrell <me@kimdcottrell.com>
2024-10-29 21:55:50 -0400

    improving examples on themes and plugins

--- wp-content/plugins/kdc-hello-world/hello-world.php
+++ wp-content/plugins/kdc-hello-world/hello-world.php
@@ -8,14 +8,60 @@
  * Text Domain:     kdc-hello-world
  */
 
-add_action( 'admin_menu', 'kdc_hello_world_add_admin_menu' );
-
-function kdc_hello_world_add_admin_menu()
-{
-      add_menu_page(
+add_action( 'admin_menu', function() {
+    add_menu_page(
         'Hello World Page', // Title of the page
         'Hello World Plugin', // Text to show on the menu link
         'manage_options', // Capability requirement to see the link
         plugin_dir_path( __FILE__ ) . 'includes/index.php' // The 'slug' - file to display when clicking the link
     );
-}
+});
+
+add_action('init', function() {
+	register_post_type('landing_page', [
+		'label' => __('Landing page', 'txtdomain'),
+		'public' => true,
+		'menu_position' => 5,
+		'menu_icon' => 'dashicons-groups', # https://developer.wordpress.org/resource/dashicons/#editor-paste-text
+		'supports' => ['title', 'editor', 'thumbnail', 'author', 'revisions', 'comments'],
+		'show_in_rest' => true, # turns on block editor support
+		'rewrite' => ['slug' => 'landing-page'],
+		'taxonomies' => ['landing_page_audience'], # declare the relationship part 1/3
+		'labels' => [
+			'singular_name' => __('Landing Page', 'txtdomain'),
+            'add_new' => __('Add New Landing Page', 'txtdomain'),
+			'add_new_item' => __('Add New Landing Page', 'txtdomain'),
+			'new_item' => __('New Landing Page', 'txtdomain'),
+			'view_item' => __('View Landing Page', 'txtdomain'),
+			'not_found' => __('No Landing Pages Found', 'txtdomain'),
+			'not_found_in_trash' => __('No Landing Pages Found in Trash', 'txtdomain'),
+			'all_items' => __('All Landing Pages', 'txtdomain'),
+			'insert_into_item' => __('Insert Into Landing Page', 'txtdomain')
+		],		
+	]);
+ 
+	register_taxonomy('landing_page_audience', ['landing_page'], [ # declare the relationship part 2/3
+		'label' => __('Audience', 'txtdomain'),
+		'hierarchical' => true,
+		'rewrite' => ['slug' => 'landing-page-audience'],
+		'show_admin_column' => true,
+		'show_in_rest' => true, # turns on block editor support
+		'labels' => [
+			'singular_name' => __('Audience', 'txtdomain'),
+			'all_items' => __('All Audience', 'txtdomain'),
+			'edit_item' => __('Edit Audience', 'txtdomain'),
+			'view_item' => __('View Audience', 'txtdomain'),
+			'update_item' => __('Update Audience', 'txtdomain'),
+			'add_new_item' => __('Add New Audience', 'txtdomain'),
+			'new_item_name' => __('New Audience Name', 'txtdomain'),
+			'search_items' => __('Search Audiences', 'txtdomain'),
+			'parent_item' => __('Parent Audience', 'txtdomain'),
+			'parent_item_colon' => __('Parent Audience:', 'txtdomain'),
+			'not_found' => __('No Audiences found', 'txtdomain'),
+		]
+	]);
+	register_taxonomy_for_object_type('landing_page_audience', 'landing_page'); # declare the relationship part 3/3
+    # yes, you really should declare that three times. https://developer.wordpress.org/reference/functions/register_taxonomy/#additional-parameter-information
+ 
+});
+
--- wp-content/themes/kdc-twentytwentyfour/functions.php
+++ wp-content/themes/kdc-twentytwentyfour/functions.php
@@ -1,32 +1,17 @@
 <?php
-# wrapping this in a class only to avoid the spam of `function kdc_ffour_` on every function declaration
-class KDC_Functions {
-    const TEXT_DOMAIN   = 'kdc-ttfour';
-    const TEXT_DOMAIN_U = 'kdc_ttfour';
+define('TEXT_DOMAIN', 'kdc-ttfour');
 
-    public function __construct() {
-        // Do nothing.
-    }
+add_action( 'wp_enqueue_scripts', function() {
+    wp_enqueue_style( 
+        TEXT_DOMAIN . '-style', 
+        get_stylesheet_uri()
+    );
 
-    public function initialize(){
-        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
-    }
-
-    public function enqueue_styles() {
-        wp_enqueue_style( 
-            self::TEXT_DOMAIN . '-style', 
-            get_stylesheet_uri()
-        );
-    
-        // add in the parent stylesheet
-        wp_enqueue_style( 
-            self::TEXT_DOMAIN . '-parent-style', 
-            get_parent_theme_file_uri( 'style.css' )
-        );
-    }
-}
-
-$Functions = new KDC_Functions();
-$Functions->initialize();
+    // add in the parent stylesheet
+    wp_enqueue_style( 
+        TEXT_DOMAIN . '-parent-style', 
+        get_parent_theme_file_uri( 'style.css' )
+    );
+});
 
 
